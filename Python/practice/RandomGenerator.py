bd9bef5bc3a18d2decce648acdb8ca46e85d6baa
jesse cole <cole.j.jesse@gmail.com>
2023-05-29 19:40:04 -0700

    password generator: input fields, can add text to password if yes is inputed

--- Python/practice/RandomGenerator.py
+++ Python/practice/RandomGenerator.py
@@ -1,12 +1,18 @@
 import random
 from random import choice, randint
 
-def randomPasswordGenerator(num):
+def randomPasswordGenerator():
+    newStringArr = ""
+    c = input("How long do you want the password to be?: ")
+    num = int(c)
+    stringLength = num
+    x = input("Want to add a word? (Something not associated with you would be recommended): ")
+    if x == "yes":
+        y = input("Ok, what would you like to add?: ")
+        newStringArr += y
+        stringLength -= len(y)
     stringArr = []
-    range = 0
-    for i in range(3):
-        if num > 
-    for i in range(2):
+    for i in range(num):
         upperCaseLetter = chr(random.randint(65,90))
         stringArr.append(upperCaseLetter)
         lowerCaseLetter = chr(random.randint(97,120))
@@ -18,13 +24,13 @@ def randomPasswordGenerator(num):
         r = choice([(33,33),(35,38),(40,41),(64,64)])
         symbol += randint(*r)
         stringArr.append(chr(symbol))
-        
-    newStringArr = []
-    for i in range(len(stringArr)):
+    for i in range(stringLength):
         temp = choice(stringArr)
-        newStringArr.append(temp)
+        newStringArr += temp
         stringArr.remove(temp)
+        if i == len(newStringArr):
+            return
 
     print(newStringArr)
-
-randomPasswordGenerator(12)
+    print(len(newStringArr))
+randomPasswordGenerator()
