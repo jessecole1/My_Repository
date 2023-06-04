629d451dbc7b1470afbe9d6d84275f66014ee7fc
jesse cole <cole.j.jesse@gmail.com>
2023-06-03 23:00:07 -0700

    javascript leetcode: reverse polish notation complete, i believe

--- javascript/leetcode/Stack/ReversePolishNotation.js
+++ javascript/leetcode/Stack/ReversePolishNotation.js
@@ -1,70 +1,44 @@
 const evalRPN = function(tokens) {
     let stack = [];
     let operators = "+ - * /";
-    let result = 0;
-
-    const add = (a , b) => {
-        intOne = parseInt(a);
-        if (b === undefined) {
-            let res = intOne;
-            result += res;
-        } else {
-            intTwo = parseInt(b);
-            console.log(intTwo);
-            result = intOne + intTwo;
-        }
-    }
-    const subtract = function(a,b) {
-        intOne = parseInt(a);
-        if (b === undefined) {
-            let res = intOne;
-            result -= res;
-        } else {
-            intTwo = parseInt(b);
-            result = intTwo - intOne;
-        }
-    }
-    const times = function(a,b) {
-        return a*b;
-    }
-    const divide = function(a,b) {
-        return a/b;
+    
+    const add = (x) => {
+        stack.push(x);
     }
 
     for (let i = 0; i < tokens.length; i++) {
+        console.log(stack);
         stack.push(tokens[i]);
-
         if (operators.includes(tokens[i])) {
-            if (tokens[i] === "+") {
-                let x = tokens[i-2];
-                let y = tokens[i-1];
-                if (isNaN(x)) {
-                    add(y, result);
-                }else {
-                    add(x,y);
+            let z = stack[stack.length-1];
+            stack.pop();
+                let x = parseInt(stack[stack.length-1]);
+                let y = parseInt(stack[stack.length-2]);
+                let c = 0;
+                if (z === "+") {
+                    c += x;
+                    c += y;
+                } else if (z === "-") {
+                    c -= x;
+                    c -= y;
+                } else if (z === "*") {
+                    c = 1;
+                    c *= x;
+                    c *= y;
+                } else if (z === "/") {
+                    c = 1;
+                    c /= x;
+                    c /= y;
                 }
-                stack.pop();
-                stack.pop();
-                stack.pop();
-            }
-            console.log("result before: " + result);
-            if (tokens[i] === "-") {
-                let x = tokens[i-2];
-                let y = tokens[i-1];
-                if (isNaN(x)) {
-                    subtract(y, result);
-                } else {
-                    subtract(x, y);
-                }
-                stack.pop();
-                stack.pop();
-                stack.pop();
-            }
+                    stack.pop();
+                    stack.pop();
+                    add(c);
+                    c = 0;
         }
     }
-    console.log(result);
-    return result;
+    console.log(stack);
+    return stack;
 
 };
 
-evalRPN(["10","10","+","5","5","-","+"]);
+evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]);
