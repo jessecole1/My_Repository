e69f9e04984cc65093d95166af6a8b47d10ef700
jesse cole <cole.j.jesse@gmail.com>
2023-06-05 22:21:20 -0700

    javascript leetcode: binarysearch, using binary search

--- javascript/leetcode/BinarySearch/BinarySearch.js
+++ javascript/leetcode/BinarySearch/BinarySearch.js
@@ -1,12 +1,20 @@
 
 
 const binarySearch = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
+    left = 0;
+    right = nums.length-1;
+    while (left <= right) {
+        mid = Math.ceil((left+right)/2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
     return -1;
 }
 
-console.log(binarySearch([-1,0,3,5,9,12], 2));
+console.log(binarySearch([-1,0,3,5,9,12], 3));
