e2203b1f5de7968846d6db0c7b88c4ab28eb6521
jesse cole <cole.j.jesse@gmail.com>
2023-06-06 19:57:59 -0700

    javascript leetcode; baseballgame for stack

---
+++ javascript/leetcode/Stack/BaseballGame.js
@@ -0,0 +1,34 @@
+
+
+const calPoints = function(operations) {
+    const parens = "+ D C";
+    let sum = 0;
+    stack = [];
+    for (let i = 0; i < operations.length; i++) {
+        if (parens.includes(operations[i])) {
+            console.log(operations[i]);
+            if (operations[i] === "+") {
+                let x = parseInt(stack[stack.length-1]);
+                let y = parseInt(stack[stack.length-2]);
+                stack.push(x + y);
+            }
+            else if (operations[i] === "D") {
+                let x = parseInt(stack[stack.length-1]);
+                stack.push(x*2);
+                console.log(stack);
+            }
+            else if (operations[i] === "C") {
+                stack.pop();
+                console.log(stack);
+            }
+        } else {
+            stack.push(parseInt(operations[i]));
+        }
+    }
+    for (let i = 0; i < stack.length; i++) {
+        sum += stack[i];
+    }
+    return sum;
+}
+
+console.log(calPoints(["1","C"]));
