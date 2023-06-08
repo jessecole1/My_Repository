23b4d698b81f8ecc89539ec7f50d52765058c7b1
jesse cole <cole.j.jesse@gmail.com>
2023-06-07 22:13:38 -0700

    portfolio website setup

---
+++ MyPortfolioWebsite/client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+
+function App() {
+
+
+  return (
+      <div className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <About />
+        <Projects />
+        <Skills />
+        <Testimonials />
+        <Contact />
+      </div>
+    );
+}
+
+export default App;
