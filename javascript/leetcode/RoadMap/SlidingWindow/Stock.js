0b5008ac4cabb8c66124401b083981b53b1743ba
jesse cole <cole.j.jesse@gmail.com>
2023-06-15 21:03:45 -0700

    javascript leetcode: bestTimeToBuyAndSellAStock

---
+++ javascript/leetcode/RoadMap/SlidingWindow/Stock.js
@@ -0,0 +1,41 @@
+
+
+const maxProfit = function(prices) {
+    
+    let left = 0;
+    let right = 1;
+
+    let maxProfitRes = 0;
+
+    while (right < prices.length) {
+        if (prices[left] < prices[right]) {
+            profit = prices[right] - prices[left];
+            if (profit > maxProfitRes) {
+                maxProfitRes = profit;
+            }
+        } else {
+            left = right;
+        }
+        right++;
+    }
+    return maxProfitRes;
+}
+
+
+console.log(maxProfit([2,1,2,1,0,0,1]));
+
+
+//          NeetCode Solution
+//          let l = 0;
+//          let r = 1;
+//          let maxP = 0;
+
+//          while (r < prices.length) {
+                // if (prices[l] < prices[r]) {
+                //     profit = prices[r] - prices[l];
+                //     maxP = maxP.max(maxP, profit);
+                // } else {
+                //     l = r;
+                // }
+                // r++;
+//          }
--- javascript/leetcode/RoadMap/TwoPointers/3Sum.js
+++ javascript/leetcode/RoadMap/TwoPointers/3Sum.js
@@ -1,24 +1,7 @@
 
 
 const threeSum = function(nums) {
-    let arr = [];
-    let i = nums.length-1;
-    for (let k = 0; k < nums.length; k++) {
-        for (let j = 0; j < nums.length; j++) {
-            console.log(nums[i], nums[k], nums[j]);
-            if (i !== j && i !== k && k !== j) {
-                if (0 === nums[i] + nums[j] + nums[k]) {
-                    arr.push(nums[i], nums[k], nums[j]);
-                }
-            }
-        }
-        i--;
-        k, j = -1;
-        if (i === 0) {
-            break;
-        }
-    }
-    return arr;
+
 }
 
 console.log(threeSum([-1,0,1,2,-1,-4]));
