6d41dede8da07fa3b0c1b6498d040de178fbf8d2
jesse cole <cole.j.jesse@gmail.com>
2023-06-18 21:28:28 -0700

    javascript leetcode: reverseLinkedList

---
+++ javascript/leetcode/RoadMap/LinkedList/ReverseLinkedList.js
@@ -0,0 +1,12 @@
+
+
+var reverseList = function(head) {
+    let prev = null;
+    while (head !== null) {
+        let nextNode = head.next;
+        head.next = prev;
+        prev = head;
+        head = nextNode;
+    }
+    return prev;
+};
