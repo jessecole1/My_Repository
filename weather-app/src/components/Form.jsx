6ffb31ab875b7fa7d9ad405f28534ffe587e1415
jesse cole <cole.j.jesse@gmail.com>
2023-06-26 23:27:08 -0700

    weather app: cleaned up autocomplete list

--- weather-app/src/components/Form.jsx
+++ weather-app/src/components/Form.jsx
@@ -21,19 +21,19 @@ const Form = (props) => {
     return (
         <div className="flex justify-center align-center bg-indigo-500 h-20 w-full">
             <h2 className="mr-4 self-center text-3xl font-bold">Weather</h2>
-            <div className="ml-4 self-center">
+            <div className="ml-4 self-center relative">
                 <form onSubmit={submitHandler}>
-                    <input type="text" value={searchCity} onChange={(e) => setSearchCity(e.target.value)}/>
+                    <span><input type="text" value={searchCity} onChange={(e) => setSearchCity(e.target.value)}/></span>
                     <button className="bg-slate-700 text-white ml-4 rounded-lg border-slate-2 p-1 pl-2 pr-2">Search</button>
-                    <div>
+                    <div className="fixed">
                         {
                             data.filter(item => {
                                 let searchTerm = searchCity.toLowerCase();
                                 let name = item.name.toLowerCase();
                                 return searchTerm && name.startsWith(searchCity);
-                            }).slice(0,10)
+                            }).slice(0,5)
                             .map((item) => {
-                                return <div className="border-b-2 border-sky-500" onClick={() => {setSearchCity(item.name); searchHandler(item.name)}} >{item.name}</div>
+                                return <div className="tracking-widest pl-4 pr-4 pt-1 pb-1 hover:bg-slate-400 bg-slate-300 cursor-pointer z-1 border-b-2 border-sky-500" onClick={() => {setSearchCity(item.name); searchHandler(item.name)}} >{item.name}</div>
                             })
                         }
                     </div>
