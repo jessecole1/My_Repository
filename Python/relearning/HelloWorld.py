b498c5a564404b3cdfaeee6be4302bffb3e98805
jesse cole <cole.j.jesse@gmail.com>
2024-04-06 23:13:39 -0700

    going over python

---
+++ Python/relearning/HelloWorld.py
@@ -0,0 +1,11 @@
+# To run the code in the terminal on Mac, type python3 -name of file- in this case, python3 HelloWorld.py
+
+print("Hello, World!")
+
+x = 50
+if (x > 50):
+    print("Bigger than 50")
+elif (x == 50):
+    print("Is 50")
+else:
+    print("Smaller than 50")
