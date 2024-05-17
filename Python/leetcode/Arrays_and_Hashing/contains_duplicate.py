abb33f597fbfd71dee80a31008d32187001113b9
jesse cole <cole.j.jesse@gmail.com>
2024-05-16 21:54:17 -0700

    python leetcode - Arrays and Hashmaps / Contains duplicate

--- Python/leetcode/Arrays_and_Hashing/contains_duplicate.py
+++ Python/leetcode/Arrays_and_Hashing/contains_duplicate.py
@@ -5,9 +5,16 @@ class Test:
     def checkArray(self, nums):
         nums_dict = {}
         for x in range(0, len(nums)):
-            nums_dict[x] = nums[x]
+            check = nums_dict.get(x)
+            print("getting x: ")
+            print(nums_dict.get(nums[x]))
+            if check:
+                print("returning false")
+                return False
+            else:
+                nums_dict[nums[x]] = x
         print(nums_dict)
 
 t = Test()
 
-t.checkArray([10,9,8,7,6])
+t.checkArray([10,9,10,8,7,6])
